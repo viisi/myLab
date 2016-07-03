1cd423e7682745311a1697c335cef2a26f07a055
tulio <marcotulio.nascimento@gmail.com>
2016-07-03 18:00:19 -0300

    added 1 - Hash de senha SHA-1 2 - Padrao para log de erros

--- myLab/src/br/com/myLab/seguranca/Authentication.java
+++ myLab/src/br/com/myLab/seguranca/Authentication.java
@@ -1,12 +1,19 @@
 package br.com.myLab.seguranca;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
+import org.jboss.logging.Logger;
 import org.picketlink.annotations.PicketLink;
 import org.picketlink.authentication.BaseAuthenticator;
 import org.picketlink.credential.DefaultLoginCredentials;
+import org.picketlink.idm.IdentityManager;
+import org.picketlink.idm.PartitionManager;
+import org.picketlink.idm.credential.Password;
 import org.picketlink.idm.model.basic.User;
 
 import br.com.myLab.entidade.Usuario;
@@ -18,20 +25,35 @@ public class Authentication extends BaseAuthenticator {
 	@Inject 
     private DefaultLoginCredentials credentials;
 	
+	@Inject
+	private PartitionManager partitionManager;
+	
 	@Inject 
     private UsuarioServico usuarioServico;
 	
 	@Inject
 	private FacesContext facesContext;
 	
+	private static final Logger log = Logger.getLogger(Authentication.class);
+	
 	@Override
 	public void authenticate() {
 		
 		try {
-			Usuario usu = usuarioServico.login(credentials.getUserId(), credentials.getPassword());
+	        String hash = getSHA1Hash(credentials.getPassword());
+			
+			Usuario usu = usuarioServico.login(credentials.getUserId(), hash);
 			if(usu != null) {
+				
+				User user = new User(usu.getLogin());
+				
 				setStatus(AuthenticationStatus.SUCCESS);
-				setAccount(new User(usu.getLogin()));
+				setAccount(user);
+				
+				IdentityManager identityManager = this.partitionManager.createIdentityManager();
+				identityManager.add(user);
+				identityManager.updateCredential(user, new Password(hash));
+
 			} else {
 				setStatus(AuthenticationStatus.FAILURE);
 				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário e/ou senha invalidos!", "");
@@ -39,7 +61,26 @@ public class Authentication extends BaseAuthenticator {
 			}
 			
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(e.getMessage(), e);
 		}
 	}
+	
+	private static String getSHA1Hash(String passwordToHash) {
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            //md.update(getSalt());
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            StringBuilder sb = new StringBuilder();
+            
+            for(int i=0; i< bytes.length ;i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+        	log.error(e.getMessage(), e);
+        }
+        return generatedPassword;
+    }
 }
